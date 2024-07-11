10ef09db6bf4a8bb4082acdbbaeb6d563343b093
junne.wanja@student.moringaschool.com <junne.wanja@student.moringaschool.com>
2024-07-11 13:19:17 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let integer = 1;
+let random = integer;
+
+let num3 = 44;
+let num4 = 10;
+let mod = num3 % num4;
+
+let number = [1, 2, 20,15,4];
+let max = Math.max(...number);
